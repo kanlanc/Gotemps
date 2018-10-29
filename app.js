aef40848e670f27797e62fefe9bd5205ed7ce22c
saivicky2015 <saivicky2015@gmail.com>
2018-10-29 12:29:16 +0530

    Final Commit before IWP review

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 
-// mongoose.connect('mongodb://localhost:27017/gotemps');
+mongoose.connect('mongodb://localhost:27017/gotemps');
 // mongoose.connect('mongodb://'+process.env.user+':'+process.env.password+'@ds161446.mlab.com:61446/gotemps');
-mongoose.connect('mongodb://'+"saivicky2015"+':'+"abc12345"+'@ds161446.mlab.com:61446/gotemps');
+// mongoose.connect('mongodb://'+"saivicky2015"+':'+"abc12345"+'@ds161446.mlab.com:61446/gotemps');
 
 mongoose.Promise = global.Promise;
 
